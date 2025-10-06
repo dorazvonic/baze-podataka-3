10538434b244cda124d89c4ccb1b6a069b333617
[Dora Zvonić] <[dorazvonic@gmail.com]>
2025-10-06 09:05:07 +0200

    cp

---
+++ video 4/add multiple employees.sql
@@ -0,0 +1,7 @@
+-- dodavanje vise podataka u tablicu
+
+
+insert into employees(employee_id, first_name, last_name, hourly_pay, hire_date, phone)
+values (1, "Pero", "Perić", 20.50, "2012-06-22", "+385995020351" ),
+	   (1, "Dora", "Zvonić", 5.50, "2012-06-22", "+385995020351" ),
+	   (1, "Toni", "Tonić", 22.50, "2012-06-22", "+385995020351" );
---
+++ video 4/add some employee.sql
@@ -0,0 +1,2 @@
+insert into employees(employee_id, phone)
+values(2, "+8593467673");
